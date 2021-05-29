1b0dffb3ddf8ab536f2e0ca8458919a244619015
Jael Oseguera <jaelosegueradavid@gmail.com>
2021-05-28 18:50:50 -0600

    Actualización Tarea

--- Ejercicios React/proyecto1/src/componentes/Persona.js
+++ Ejercicios React/proyecto1/src/componentes/Persona.js
@@ -1,22 +1,26 @@
-//seccion de importacion de componentes
+//seccion de importancion de componentes
 import React from "react";
 
 export default function Persona(props){
-
-    console.log(props);
+    
+    //Asignacion por destructuring
+    const {personaInfo, fnsaludar} = props;
+    const {nombre = "No disponible", apellido = "No disponible"} = personaInfo;  //props por defecto
+    
+    console.log(nombre); 
 
     return(
         <div>
             <h2>Datos Personales</h2>
             <table>
                 <tbody>
-                    <tr> <td>Nombre:      </td><td> {props.personainfo.nombre}       </td> </tr>
-                    <tr> <td>Apellidos:   </td><td> {props.personainfo.apellido}     </td> </tr>
-                    <tr> <td>edad:        </td><td> {props.personainfo.edad}         </td> </tr>
-                    <tr> <td>nacionalidad:</td><td> {props.personainfo.nacionalidad} </td> </tr>
-                    <tr> <td>genero:      </td><td> {props.personainfo.genero}       </td> </tr>
+                    <tr> <td>Nombre:    </td>   <td> {nombre} </td>      </tr>
+                    <tr> <td>Apellidos: </td>   <td> {apellido}</td>  </tr>
+                    <tr> <td>edad:      </td>   <td> {personaInfo.edad}</td>  </tr>
+                    <tr> <td>nacionalidad:</td> <td> {personaInfo.nacionalidad}</td>  </tr>
+                    <tr> <td>genero:      </td> <td> {personaInfo.genero}</td>  </tr>
                     <tr> <td>
-                        <button onclick={ () => props.fnsaludar(props.personainfo.nombre) } >Saludar</button>      
+                            <button onClick={ () => fnsaludar(personaInfo.nombre) } >Saludar</button>
                     </td> </tr>
                 </tbody>
             </table>
@@ -27,10 +31,8 @@ export default function Persona(props){
 export function Intereses(){
 
     return(
-<div>
-<h2>Estos son intereses</h2>
-
-</div>
-
+        <div>
+            <h2>Estos son intereses</h2>
+        </div>
     )
 }
